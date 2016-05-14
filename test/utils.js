b15784225924513ce7d00f15e8836a0361a32535
lukasz.guzik <lukasz.guzik@schibsted.pl>
2016-05-14 13:02:17 +0200

    Add tests for arrow functions

--- test/utils.js
+++ test/utils.js
@@ -14,11 +14,20 @@ describe('utils', function() {
       var func1 = function() {};
       var func2 = function(a) {};
       var func3 = function(a, bc, defg) {};
+      var func4 = () => {};
+      var func5 = (a) => {};
+      var func6 = (a, bc, defg) => {};
+      var func7 = (a, bc, /* some comment */ defg) => {};
       utils.getParameters(func1).should.eql([]);
       utils.getParameters(func2).should.eql(['a']);
       utils.getParameters(func3).should.eql(['a', 'bc', 'defg']);
+      utils.getParameters(func4).should.eql([]);
+      utils.getParameters(func5).should.eql(['a']);
+      utils.getParameters(func6).should.eql(['a', 'bc', 'defg']);
+      utils.getParameters(func7).should.eql(['a', 'bc', 'defg']);
       // Test cache
       utils.getParameters(func3).should.eql(['a', 'bc', 'defg']);
+      utils.getParameters(func6).should.eql(['a', 'bc', 'defg']);
     });
   });
 
